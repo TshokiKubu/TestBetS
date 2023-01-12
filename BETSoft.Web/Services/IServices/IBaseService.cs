a7ecc2736316acc2e0f9b19c3dcef6a724fb4c1a
TshokiKubu <Baitshokikubu@gmail.com>
2023-01-12 02:21:06 -0800

    Add project files.

---
+++ BETSoft.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using BETSoft.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BETSoft.Web.Services.IServices
+{
+    public interface IBaseService: IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
---
+++ BETSoft.Web/Services/IServices/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using BETSoft.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BETSoft.Web.Services.IServices
+{
+    public interface ICouponService
+    {
+        Task<T> GetCoupon<T>(string couponCode, string token = null);
+
+    }
+}
